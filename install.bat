208a4f5eff5674ecf40ba11f12cd4650b99a6569
delusionx <spaghetti90314@gmail.com>
2020-01-06 11:10:27 -0700

    Installer & run scripts

---
+++ install.bat
@@ -0,0 +1,1 @@
+python -m venv venv && cd "venv/scripts" && activate.bat && cd "../../" && pip install -r requirements.txt
---
+++ start.bat
@@ -0,0 +1,1 @@
+cd "venv/scripts" && activate.bat && cd "../../" && livesearch.py
